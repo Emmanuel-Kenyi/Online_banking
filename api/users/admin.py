c6b492d51ecba7d36ca0954c41f09a5c9f6644e9
Emmanuel Kenyi Samuel <kenyilogworit@gmail.com>
2025-07-18 19:53:21 +0300

    django

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 # Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'is_admin', 'has_voted', 'is_staff', 'is_superuser')
+    list_filter = ('is_admin', 'has_voted', 'is_staff', 'is_superuser')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('is_admin', 'has_voted')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('is_admin', 'has_voted')}),
+    )
