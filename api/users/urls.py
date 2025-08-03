9800d204cbfc11faed4ed523153c60ad8517f116
Emmanuel Kenyi Samuel <kenyilogworit@gmail.com>
2025-08-03 20:07:06 +0300

    django

--- api/users/urls.py
+++ api/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('user/', views.current_user, name='current-user'),  # ✅ New endpoint to fetch current user info
+    path('user/', views.current_user, name='current-user'),  
     path('register/', views.register_user, name='register'),
     path('profile/', views.user_profile, name='user-profile'),
     path('count/', views.user_count, name='user_count'),
